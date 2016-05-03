72035d0e46e760cd4ff350f2dd6923f60ab23d64
HJ Cross <github@chimpjuice.com>
2016-05-03 18:52:43 -0400

    update run script port

--- _run.sh
+++ _run.sh
@@ -5,4 +5,4 @@ docker stop some-app
 docker rm some-mongo
 docker rm some-app
 docker run --name some-mongo -d mongo
-docker run --name some-app -d -p 0.0.0.0:80:8080 --link some-mongo:mongo docker_app.dockerfile
+docker run --name some-app -d -p 0.0.0.0:80:80 --link some-mongo:mongo docker_app.dockerfile
