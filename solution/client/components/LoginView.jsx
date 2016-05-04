ab2d7327cf96a589a94396547287444fadae7dcf
HJ Cross <github@chimpjuice.com>
2016-05-03 20:09:32 -0400

    more linter issues

--- solution/client/components/LoginView.jsx
+++ solution/client/components/LoginView.jsx
@@ -1,23 +1,14 @@
 import React, {PropTypes} from 'react';
-import Button from './shared/Button.jsx'
-import Field from './shared/Field.jsx'
+import Button from './shared/Button.jsx';
+import Field from './shared/Field.jsx';
 
-export default class LoginView extends React.Component {
-
-  constructor(props) {
-    super(props);
-  }
-
-  render() {
-    return (
-      <div className="login">
-        <div>
-          <Field label='Name' />
-          <Field label='Password' />
-          <Button text='Sign In' />
-          <Button text='Register' />
-        </div>
-      </div>
-    );
-  }
+function LoginView() {
+  return (
+    <div className="login-view">
+        <Field label="Name" />
+        <Field label="Password" />
+        <Button text="Sign In" />
+        <Button text="Register" />
+    </div>
+  );
 }
--- solution/client/components/MainContainer.jsx
+++ solution/client/components/MainContainer.jsx
@@ -1,27 +1,23 @@
-import React, {PropTypes} from 'react';
+import React from 'react';
 
-import ContactView from './ContactView.jsx'
-import ListView from './ListView.jsx'
-import LoginView from './LoginView.jsx'
-import TopMenu from './TopMenu.jsx'
-import BottomMenu from './BottomMenu.jsx'
+import ContactView from './ContactView.jsx';
+import ListView from './ListView.jsx';
+import LoginView from './LoginView.jsx';
+import TopMenu from './TopMenu.jsx';
+import BottomMenu from './BottomMenu.jsx';
 
-export default class MainContainer extends React.Component {
-
-  constructor(props) {
-    super(props);
-  }
-
-  render() {
-    return (
-      <div className="container">
-        <TopMenu />
-        <ContactView />
-        <LoginView />
-        <ListView />
-        <BottomMenu />
-      </div>
-    );
-  }
+function MainContainer() {
+  return (
+    <div className="container">
+      <TopMenu />
+      <ContactView />
+      <LoginView />
+      <ListView />
+      <BottomMenu />
+    </div>
+  );
 }
+
+
+export default MainContainer;
 
