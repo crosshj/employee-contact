57b5132a4023d4aaea535db22df40d5fefd2e5af
HJ Cross <github@chimpjuice.com>
2016-05-03 20:14:13 -0400

    nagging linter issues, wish I had Atom set up

--- solution/client/components/LoginView.jsx
+++ solution/client/components/LoginView.jsx
@@ -1,14 +1,16 @@
-import React, {PropTypes} from 'react';
+import React from 'react';
 import Button from './shared/Button.jsx';
 import Field from './shared/Field.jsx';
 
 function LoginView() {
   return (
     <div className="login-view">
-        <Field label="Name" />
-        <Field label="Password" />
-        <Button text="Sign In" />
-        <Button text="Register" />
+      <Field label="Name" />
+      <Field label="Password" />
+      <Button text="Sign In" />
+      <Button text="Register" />
     </div>
   );
 }
+
+export default LoginView;
